6f2b003b9caa08256eceb9669057b49fbf65f847
jonn194 <jmsalice@hotmail.com>
2025-04-01 10:51:16 +0900

    Added previews to movie cover uploads

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Moviesscores
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.assets.paths << Rails.root.join("app", "assets", "images", "levers")
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
